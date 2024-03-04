09a8040c305ced6144c95189cc882b3ce554c87b
nathan <nathankdgray@gmail.com>
2024-03-04 07:04:06 -0800

    added directory functions
    - create functions to load image data and label them
    - directory needs to be created and added with training data

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -3,3 +3,17 @@ import cv2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def load_and_preprocess_images(directory, target_size = (224, 224)):
+    images = []
+    labels = []
+
+    # for subdirectoy of labeled image data of finger count
+    for label in os.listdir(directory):
+        label_dir = os.path.join(directory, label)
+
+        # skip any files in directory and only process in subdirectory folders
+        if not os.path.isdir(label_dir):
+            continue
+
+    for image_file in os.listdir(label_dir):
+        image_path = os.path.join(label_dir, image_file)
+        image = cv2.imread(image_path)
